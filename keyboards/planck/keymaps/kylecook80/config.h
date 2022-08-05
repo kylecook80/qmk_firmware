9ff91bf816f0c6161e7734ce3c9a8a7a540676af
Kyle Cook <kcook@rivian.com>
2022-08-05 12:56:33 -0500

    Added keymap for planck

--- keyboards/planck/keymaps/kylecook80/config.h
+++ keyboards/planck/keymaps/kylecook80/config.h
@@ -16,12 +16,14 @@
 
 #pragma once
 
+#define AUDIO_INIT_DELAY
+
 #ifdef AUDIO_ENABLE
 #    define STARTUP_SONG SONG(PLANCK_SOUND)
 // #define STARTUP_SONG SONG(NO_SOUND)
 
 #    define DEFAULT_LAYER_SONGS \
-        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
+        { SONG(QWERTY_SOUND) }
 #endif
 
 /*
